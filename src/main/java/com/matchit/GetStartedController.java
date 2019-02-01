3a35d2ee9206f95b5fba7520a6b8ec880a8acc8d
MotamenGhanoum <ghanoummotamen@gmail.com>
2019-02-01 11:24:01 +0100

    signup controller

--- src/main/java/com/matchit/GetStartedController.java
+++ src/main/java/com/matchit/GetStartedController.java
@@ -41,6 +41,7 @@ public class GetStartedController implements Initializable {
 
 
 
+
     }
 
 }
--- src/main/java/com/matchit/SignUpController.java
+++ src/main/java/com/matchit/SignUpController.java
@@ -1,17 +1,27 @@
 package com.matchit;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SignUpController implements Initializable {
 
+    public void SignUpButton() {
+        Button signUpB = new Button();
 
+        signUpB.setOnAction(event -> {
+            String name, email, password;
+            
+        });
+
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
-
