823844f5c30166dc65526aa302a6b07f601908ca
MotamenGhanoum <ghanoummotamen@gmail.com>
2019-02-03 17:45:05 +0100

    SecurityManagementPackge

--- src/main/java/com/matchit/SignUpController.java
+++ src/main/java/com/matchit/SignUpController.java
@@ -3,18 +3,45 @@ package com.matchit;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static javafx.application.Application.launch;
+
 public class SignUpController implements Initializable {
+    @FXML
+    private AnchorPane BK;
+
+    @FXML
+    private AnchorPane singUpBK;
+
+    @FXML
+    private TextField nameSignUp;
+
+    @FXML
+    private TextField emailSignUp;
+
+    @FXML
+    private TextField passSignUp;
+
+    @FXML
+    private Button signUpB;
+
+
+
 
     public void SignUpButton() {
         Button signUpB = new Button();
 
         signUpB.setOnAction(event -> {
             String name, email, password;
-            
+
+            name = nameSignUp.getText().toString();
+            email=emailSignUp.getText().toString();
+            password=passSignUp.getText().toString();
         });
 
 
@@ -24,4 +51,6 @@ public class SignUpController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+
 }
