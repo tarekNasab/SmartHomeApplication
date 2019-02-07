31b90a4f99e71cf62c19d8630467caa9bc947010
Malek Al Harani <malek.horani432@hotmail.com>
2019-02-07 13:14:20 +0100

    Login admin forum updated 2

--- src/main/java/com/matchit/Controllers/LogInAsAdminController.java
+++ src/main/java/com/matchit/Controllers/LogInAsAdminController.java
@@ -1,4 +1,33 @@
 package com.matchit.Controllers;
 
-public class LogInAsAdminController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogInAsAdminController implements Initializable {
+
+    @FXML
+    public TextField adminUserName;
+    @FXML
+    public PasswordField adminPassword;
+    @FXML
+    public Button logInAdminButton;
+    @FXML
+    public Label goBackLabel;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+
 }
