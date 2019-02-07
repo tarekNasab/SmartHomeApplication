1b3e7b67624f3b3e892c51d3b25ed7959e9037b1
MotamenGhanoum <ghanoummotamen@gmail.com>
2019-02-07 13:17:04 +0100

    worked on sign on admain

--- src/main/java/com/matchit/Controllers/LogInAsAdminController.java
+++ src/main/java/com/matchit/Controllers/LogInAsAdminController.java
@@ -1,4 +1,7 @@
 package com.matchit.Controllers;
 
+import com.matchit.User.Admin;
+
 public class LogInAsAdminController {
+    Admin admin=new Admin();
 }
--- src/main/java/com/matchit/User/Admin.java
+++ src/main/java/com/matchit/User/Admin.java
@@ -3,6 +3,8 @@ package com.matchit.User;
 import com.matchit.DB_Connection.ConnectionConfig;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class Admin {
@@ -34,6 +36,30 @@ public class Admin {
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+        }
+    }
+
+
+
+    public void logInAdmain(String enteredEmail , String eneteredPass) throws SQLException {
+
+        String logInQueryAd = "SELECT * FROM [admain] WHERE email = ? AND password = ?" ;
+        PreparedStatement logInStatement = ConnectionConfig.prepareStatement(logInQueryAd);
+
+        logInStatement.setString(1 , enteredEmail);
+        logInStatement.setString(2 , eneteredPass);
+
+        ResultSet admainInfoSet = logInStatement.executeQuery();
+        ResultSetMetaData admainInfoSetMD = admainInfoSet.getMetaData();
+
+
+
+        if (admainInfoSet.next()){
+            System.out.println("admain can log in");
+        }
+
+        else if (!admainInfoSet.isBeforeFirst()){
+            System.out.println("admain can not log in");
         }
     }
 
