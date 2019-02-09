161eaf30f28423fc0118ab951bc33e7efa4e145f
Malek Al Harani <malek.horani432@hotmail.com>
2019-02-09 21:55:41 +0100

    User dashboard updated, worked on User profile

--- src/main/java/com/matchit/Controllers/DashBoardUserController.java
+++ src/main/java/com/matchit/Controllers/DashBoardUserController.java
@@ -1,14 +1,14 @@
 package com.matchit.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -16,7 +16,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -24,47 +23,52 @@ import java.util.ResourceBundle;
 
 public class DashBoardUserController implements Initializable {
 
-
-
-    @FXML
-    private Label welcome;
-    @FXML
-    private Button securityButton;
+    ObservableList<MenuItem> profileMenuItems = FXCollections.observableArrayList();
 
     @FXML
-    private Label cosumizeLabel;
+    public AnchorPane dbBK;
+    @FXML
+    public Label cosumizeLabel;
+    @FXML
+    public ToggleButton autoModeOn;
+    @FXML
+    public ImageView dashboardLogo;
+    @FXML
+    public Pane rightSidePanBoard;
+    @FXML
+    public GridPane imageGridView;
+    @FXML
+    public Button heatingButton;
+    @FXML
+    public Button securityButton;
+    @FXML
+    public Button lightButton;
+    @FXML
+    public Button kitchenButton;
+    @FXML
+    public Label dashboard;
+    @FXML
+    public Button profileAvatar;
+    @FXML
+    public Label goBackLabel;
+    @FXML
+    public Pane rightSidePanProfile;
+    @FXML
+    public Pane profileBodyPane;
+    @FXML
+    public Pane topBanner;
+    @FXML
+    public MenuButton profileMenuB;
+    @FXML
+    public Label welcome;
 
-    @FXML
-    private GridPane imageGridView;
-
-    @FXML
-    private Button kitchenButton;
-
-    @FXML
-    private ImageView profile;
-
-    @FXML
-    private Pane rightSidePan;
-
-    @FXML
-    private AnchorPane dbBK;
-
-    @FXML
-    private ToggleButton autoModeOn;
-
-    @FXML
-    private Button heatingButton;
-
-    @FXML
-    private Button lightButton;
-
-    @FXML
-    private ToggleButton autoModeOff;
-    @FXML
-    private Label goBackLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        setActionsMenuProfile(profileMenuB);
+        rightSidePanProfile.setVisible(false);
+        rightSidePanBoard.setVisible(false);
 
         try {
             setWelcomUser(LogInController.getInctance().userName());
@@ -83,9 +87,34 @@ public class DashBoardUserController implements Initializable {
 
     }
 
-    public void setWelcomUser( String user){
-         this.welcome.setText("Welcome," + " "+ user);
+    public void setWelcomUser(String user) {
+        this.welcome.setText("Welcome," + " " + user);
+    }
+
+    public void onDashBoardShowRightPane(MouseEvent showBoardPaneEvent) {
+        rightSidePanProfile.setVisible(false);
+        rightSidePanBoard.setVisible(true);
+
+    }
+
+    public void showProfilePane(MouseEvent showProfileEvent) {
+        rightSidePanBoard.setVisible(false);
+        rightSidePanProfile.setVisible(true);
     }
 
 
+    public void setActionsMenuProfile(MenuButton menuButton) {
+
+
+        menuButton.getItems().clear();
+        profileMenuItems.removeAll();
+
+
+        MenuItem showProfile = new MenuItem("My profile");
+        MenuItem editName = new MenuItem("Change my name");
+        MenuItem editEmail = new MenuItem("Change my email");
+
+        profileMenuItems.addAll(showProfile, editEmail, editName);
+        menuButton.getItems().addAll(profileMenuItems);
+    }
 }
